abbe2262fc3279df6db0d9386d3df5d46c53d5e1
John Steven Bernal Gonzalez <bernalgonzalezjohn@gmail.com>
2021-11-17 18:36:50 -0500

    Configuracion Inicial con Java SprintBoot - conneccion con MongoDB

---
+++ src/main/java/com/IcavMisionTic/Account_ms_Icav/AccountMsIcavApplication.java
@@ -0,0 +1,13 @@
+package com.IcavMisionTic.Account_ms_Icav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMsIcavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMsIcavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IcavMisionTic/Account_ms_Icav/AccountMsIcavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IcavMisionTic.Account_ms_Icav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMsIcavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
